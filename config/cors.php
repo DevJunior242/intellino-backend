e38c9fc4dee6933ee3d45ff387594fcd52baf04a
DevJunior242 <devjunior242@gmail.com>
2026-04-11 17:27:39 +0000

    Update allowed_origins in CORS configuration

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://intellino-frontend-qp8yo0fck-devcodeur.vercel.app'],
+    'allowed_origins' => ['https://intellino-frontend-qb45m86f7-devcodeur.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
