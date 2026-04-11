1e6c994f405205c7386daabdb5d464e6c34e5dd2
DevCodeur <devjunior677@gmail.com>
2026-04-11 15:29:43 +0000

    fix: SSL verify false

--- config/database.php
+++ config/database.php
@@ -58,11 +58,10 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-           'options' => extension_loaded('pdo_mysql') ? array_filter([
-                  PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-          ]) : [],
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+            ]) : [],
         ],
-
         'mariadb' => [
             'driver' => 'mariadb',
             'url' => env('DB_URL'),
@@ -148,7 +147,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-database-'),
+            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')) . '-database-'),
             'persistent' => env('REDIS_PERSISTENT', false),
         ],
 
